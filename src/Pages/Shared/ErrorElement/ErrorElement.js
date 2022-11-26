cf07daac763ca8662ee9ce86d1d05cdc898bc40b
atikdev-bd <atikdev.bd@gmail.com>
2022-11-26 23:03:45 +0600

    404 element added

--- src/Pages/Shared/ErrorElement/ErrorElement.js
+++ src/Pages/Shared/ErrorElement/ErrorElement.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import NotFound from '../../../Assets/404/404-error-with-character-error-design-template-vector-20568716.jpg'
+
 const ErrorElement = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center items-center'>
+           <div>
+            <img className='w-96' src={NotFound} alt="404 Page" />
+            </div> 
         </div>
     );
 };
